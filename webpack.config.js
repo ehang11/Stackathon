05b36f626b43742a56fd52330b1f256c49038675
Eric Hang <ehang11@gmail.com>
2022-03-05 08:16:23 -0500

    configs

--- webpack.config.js
+++ webpack.config.js
@@ -16,9 +16,9 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        // options: {
-        //   presets: ['@babel/preset-react'],
-        // },
+        options: {
+          presets: ['@babel/preset-react'],
+        },
       },
       {
         test: /\.html$/,
